4a7d96ccf38adf3a03bd9f3422d90b859d947d5a
maryam <Reemaali9048@gmail.com>
2023-10-28 03:12:40 +0300

    Home.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Navbar from"./Navbar";
 
 function App() {
   return (
+
     <div>
       <Article />
       <Comment />
+      <Navbar/>
     </div>
   );
 }
